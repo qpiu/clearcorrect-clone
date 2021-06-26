5ed4fcc776120fb73c4013d6591e2de69a4fa13f
jim <jimchou223@gmail.com>
2021-06-26 17:31:19 +0800

    arrange post

--- pages/blog.js
+++ pages/blog.js
@@ -1,38 +1,47 @@
-import Head from 'next/head'
-import Post from '../components/Post';
-import { useState, useEffect } from 'react';
-import { getAllPostsFromServer } from '../lib/utils';
+import Head from "next/head";
+import Post from "../components/Post";
+import { useState, useEffect } from "react";
+import { getAllPostsFromServer } from "../lib/utils";
+import Hero from "../sections/Hero";
+import HeroBelt from "../sections/HeroBelt";
+export default function Blog() {
+  const [posts, setPosts] = useState([]);
+  useEffect(async () => {
+    let mounted = true;
+    if (mounted) {
+      const postsFromServer = await getAllPostsFromServer();
+      setPosts(postsFromServer);
+    }
+    return () => (mounted = false);
+  }, []);
 
-export default function Blog () {
-    const [ posts, setPosts ] = useState( [] );
-    useEffect( async () => {
-        let mounted = true;
-        if ( mounted ) {
-            const postsFromServer = await getAllPostsFromServer();
-            setPosts( postsFromServer );
-        }
-        return () => ( mounted = false );
-    }, [] );
+  return (
+    <div>
+      <Head>
+        <title>ClearCorrect</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Hero title="Health & Orthodontics" page="journey"></Hero>
 
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-            <Head>
-                <title>ClearCorrect</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main className="flex flex-col items-center flex-1 px-20 py-10">
-                { posts && (
-                    <div className="grid grid-cols-2 gap-5">
-                        { posts.map( ( post, id ) => {
-                            return (
-                                <div key={ id }>
-                                    <Post post={ post } />
-                                </div>
-                            );
-                        } ) }
+      <HeroBelt></HeroBelt>
+      <main className="ftca-section ">
+        <div className="container">
+          <div className="flex">
+            {posts && (
+              <div className="blogs">
+                {posts.map((post, id) => {
+                  return (
+                    <div className="blog" key={id}>
+                      <Post post={post} />
                     </div>
-                ) }
-            </main>
+                  );
+                })}
+              </div>
+            )}
+            <div className="blog-side">i am blog side</div>
+          </div>
         </div>
-    );
+      </main>
+    </div>
+  );
 }
