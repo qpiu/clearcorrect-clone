d5548e2ff9c8d896966fe91d982b47149050738a
qpiu <qpiu.qpiu@gmail.com>
2021-06-26 21:39:26 +0800

    Modify map and add autocomplete input field.

--- components/Map.js
+++ components/Map.js
@@ -1,50 +1,55 @@
-import { React } from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { useState, useEffect } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
+//import { Wrapper } from '@googlemaps/react-wrapper';
 import { GOOG_MAPS_API_KEY } from '../lib/constants';
+import MapSearchBar from './MapSearchBar';
 
-const containerStyle = {
+const mapStyle = {
     width: '500px',
     height: '500px',
     margin: '0 auto'
 };
-//const google = window.google ? window.google : {};
-// const options = {
-//     zoomControlOptions: {
-//         position: google.maps.ControlPosition.RIGHT_CENTER,
-//         // ...otherOptions
-//     }
-// }
 
-const center = {
+const loader = new Loader( {
+    apiKey: `${ GOOG_MAPS_API_KEY }`,
+    version: "weekly",
+    libraries: [ "places" ]
+} )
+
+const centerTW = {
     lat: 24,
     lng: 121
 };
 
+const mapOptions = {
+    center: centerTW,
+    zoom: 8
+};
+
 export default function Map () {
-    //const [ map, setMap ] = useState( null )
-
-    // const onLoad = useCallback( function callback ( map ) {
-    //     const bounds = new window.google.maps.LatLngBounds();
-    //     map.fitBounds( bounds );
-    //     setMap( map )
-    // }, [] )
-
-    // const onUnmount = useCallback( function callback ( map ) {
-    //     setMap( null )
-    // }, [] )
+    const [ googlemap, setGoogleMap ] = useState( null )
+    //const [ autocomplete, setAutocomplete ] = useState( null )
+    useEffect( () => {
+        let mounted = true;
+        if ( mounted ) {
+            loader
+                .load()
+                .then( ( google ) => {
+                    setGoogleMap( google );
+                    new google.maps.Map( document.getElementById( "map" ), mapOptions );
+                    //setAutocomplete( new google.maps.places.AutocompleteService() );
+                } )
+                .catch( ( e ) => console.log( e ) )
+        }
+        return () => {
+            mounted = false;
+        };
+    }, [] );
 
     return (
-        <LoadScript
-            googleMapsApiKey={ `${ GOOG_MAPS_API_KEY }` }
-        >
-            <GoogleMap
-                mapContainerStyle={ containerStyle }
-                center={ center }
-                zoom={ 8 }
-            >
-                { /* Child components, such as markers, info windows, etc. */ }
-                <></>
-            </GoogleMap>
-        </LoadScript>
-    )
+        <div>
+            <MapSearchBar />
+            <div id="map" style={ mapStyle }></div>
+        </div>
+    );
 }
