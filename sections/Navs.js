eeef4a8b4cc43b90c11b6226e2c2066567a4d41e
qpiu <qpiu.qpiu@gmail.com>
2021-07-03 23:15:32 +0800

    Fix navbar bug.

--- sections/Navs.js
+++ sections/Navs.js
@@ -2,25 +2,26 @@ import React, { useState } from "react";
 import Nav from "../components/Nav";
 import styles from "../sections/Navs.module.css";
 const title = [
-  { title: "Who we are", href: "/whoweare.html" },
+  { title: "Who we are", href: "/whoweare" },
   { title: "ClearCorrect", href: "/" },
-  { title: "Your ClearCorrect Journey", href: "/journey.html" },
-  { title: "Health and Orthodontics", href: "/blog.html" },
-  { title: "Get in Touch", href: "/contact.html" },
+  { title: "Your ClearCorrect Journey", href: "/journey" },
+  { title: "Health and Orthodontics", href: "/blog" },
+  { title: "Get in Touch", href: "/contact" },
 ];
 
-export default function Navs(props) {
-  const [toggle, setToggle] = useState(false);
+export default function Navs ( props ) {
+  const [ toggle, setToggle ] = useState( false );
 
   return (
-    <ul className={styles.navWrapper}>
-      <button onClick={() => setToggle((toggle) => !toggle)} className={styles.toggleButton}>
+    <ul className={ styles.navWrapper }>
+      <button onClick={ () => setToggle( ( toggle ) => !toggle ) } className={ styles.toggleButton }>
         <i className="fas fa-list"></i>Menu
       </button>
-      <div className={toggle ? `${styles.navList} ${styles.open}` : styles.navList}>
-        {title.map((el, i) => {
-          return <Nav active={props.currentRoute === el.href} el={el} key={i}></Nav>;
-        })}
+      <div className={ toggle ? `${ styles.navList } ${ styles.open }` : styles.navList }>
+        { title.map( ( el, i ) => {
+          console.log( props.currentRoute )
+          return <Nav active={ props.currentRoute === el.href } el={ el } key={ i }></Nav>;
+        } ) }
       </div>
     </ul>
   );
