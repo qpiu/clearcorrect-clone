13b7c9b5bd2baeb8974dca02105ff6cf6278a799
jim <jimchou223@gmail.com>
2021-08-18 21:35:25 +0800

    fix typo

--- sections/WcaList.js
+++ sections/WcaList.js
@@ -4,7 +4,7 @@ import WcaItem from "../components/WcaItem";
 const list = [
   { src: "comfortable-icon.png", txt: "配戴舒適" },
   { src: "removable-icon.png", txt: "簡單摘戴" },
-  { src: "invisible-icon.png", txt: "簡單摘戴" },
+  { src: "invisible-icon.png", txt: "透明美觀" },
 ];
 
 export default function WcaList () {
