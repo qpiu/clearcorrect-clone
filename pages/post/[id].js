866fa322b6cbf328c2b08d7677a46fc14f438c96
qpiu <15633673+qpiu@users.noreply.github.com>
2022-04-22 22:05:21 +0800

    test

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -49,6 +49,7 @@ export async function getStaticPaths () {
     res = await axios.get( POSTS_API_URL );
   } catch ( error ) {
     console.log( error );
+    return { paths: [], fallback: false };
   }
   const posts = res.data;
   // Get the paths we want to pre-render based on posts
